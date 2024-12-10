3763cb830e491700267f4d71970a65e4d2b8cb6f
Mallart <matheo.allart09@gmail.com>
2024-12-10 12:08:50 +0100

    renamed a file and added comments and laucher for Windows

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-# Put all your functions here
+# Put all your functions here, or import them from other files
 
-
+# putting the type for your parameters is mandatory
 def modulo(expr:str, mod:int):
     return eval(expr) % mod
 
@@ -10,7 +10,9 @@ functions = [
     { "option": "Evaluate expression\n",                  "func":eval },
 ]
 
+# add an entry to the avaiable functions (for the launcher)
 def add_option(function, description):
     functions.append({"option": description, "func":function})
-    
+
+# add your functions like this
 add_option(modulo, "Compute modulo")
--- launch.py
+++ launch.py
@@ -6,12 +6,18 @@ from functions import *
 def pres_options(opt_func):
     
     print("Please choose an option:\n")
+    # display all available functions
     [print(str(i) + " > " + opt_func[i]["option"] + "\n") for i in range(len(opt_func))]
     choice=int(input("\t>"))
+    # this will retrieve all the required parameters for your functions and their names
     p = {name: param.annotation for name, param in signature(opt_func[choice]["func"]).parameters.items()}
     if(len(opt_func) >= choice):
+        # query user input for each parameter
         params = [(type)(input("\nEnter param \"" + str(name) + "\" >")) for name, type in p.items()]
+        # execute function
         print((opt_func[choice]["func"])(*params))
+    else:
+        print("The chosen index is out of bounds. Shutting down.")
     return
     
     
